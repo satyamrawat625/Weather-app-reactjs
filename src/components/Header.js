762f8b06899bc2be56f4acbb3e3861eae14e7026
Satyam Rawat <satyamrawat2048@gmail.com>
2023-07-03 15:15:44 +0530

    First commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div >
+
+    <div className="flexCenter h-container">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Assumenda, similique.
+    </div>
+      
+    </div>
+  )
+}
+
+export default Header
+
---
+++ src/components/current-weather/current-weather.js
@@ -0,0 +1,49 @@
+import React from "react"
+import "./current-weather.css"
+
+const CurrentWeather = ({data}) => {
+    return (
+        <div className="weather">
+            <div className="top">
+                <div>
+                    <p className="city">{data.city}</p>
+                    <p className="weather-description">{data.weather[0].description}</p>
+                </div>
+                <img src={`icons/${data.weather[0].icon}.png`} alt="weather-icon" className="weather-icon" />
+            </div>
+
+
+            <div className="bottom">
+                <div className="temperature">{Math.round(data.main.temp)}°C</div>
+
+                <div className="details">
+                    <div className="parameter-row">
+                        <span className="parameter-label center">Details :</span>
+                    </div>
+
+                    <div className="parameter-row">
+                        <span className="parameter-label">Feels like</span>
+                        <span className="parameter-value">{Math.round(data.main.temp)}°C</span>
+                    </div>
+
+                    <div className="parameter-row">
+                        <span className="parameter-label">Wind</span>
+                        <span className="parameter-value">{data.wind.speed} m/s</span>
+                    </div>
+
+                    <div className="parameter-row">
+                        <span className="parameter-label">Humidity</span>
+                        <span className="parameter-value">{Math.round(data.main.humidity)}%</span>
+                    </div>
+
+                    <div className="parameter-row">
+                        <span className="parameter-label">Pressure</span>
+                        <span className="parameter-value">{Math.round(data.main.pressure)} atm</span>
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default CurrentWeather
